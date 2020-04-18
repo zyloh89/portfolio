466dde178cd5d6b7a5b1df625880cd6d440b72dc
Ziyen <zyloh89@gmail.com>
2020-04-18 22:32:34 +0930

    add updated porftolio

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# portfolio
+## Portfolio Website
+
+This is an update to my previous portfolio website, utilising HTML, CSS and Bootstrap framework. 
+The website is created as a mobile-first website.
+
+## Deployed link
+
+## Screenshots
---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(e){
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    // resize event
+    $win.resize(function(){
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function(e){
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+function toggle_onclick($win, $navbar, width){
+    if($win.width() <= 768){
+        $navbar.css({left: `-${width}px`});
+    }else{
+        $navbar.css({left: '0px'});
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
